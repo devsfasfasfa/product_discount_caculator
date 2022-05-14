a7d571ea803266fd10a71686f7040da3efb8a359
thao-dev <thaotoiday@gmail.com>
2022-05-14 16:30:13 +0700

    upload

---
+++ display_discount.php
@@ -0,0 +1,49 @@
+<?php
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Discount Product</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+
+
+    <div id="app">
+
+        <h1>Product Discount Caculator</h1>
+
+        <label for="">Product Decscription:</label>
+        <span></span>
+
+        <label for="">List Price:</label>
+        <span></span>
+
+        <label for="">Standard Discount:</label>
+        <span></span>
+
+        <label for="">Discount Amount:</label>
+        <span></span>
+
+        <label for="">Discount Price:</label>
+        <span></span>
+
+
+    </div>
+
+
+</body>
+
+</html>
